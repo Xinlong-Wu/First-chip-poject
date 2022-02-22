0469c539e6ab0c99b03d869cfb3111d54a4e09a2
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-22 22:28:34 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  22:28:34 up  3:40,  1 user,  load average: 1.38, 1.22, 1.14

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-STUID = ysyx_22040000
-STUNAME = 张三
+STUID = ysyx_22040200
+STUNAME = 乌鑫龙
 
-TOP_MOD = GCD.v
+NVBOARD_HOME = $(shell pwd)/nvboard
+INC_PATH ?=
+
+TOPNAME = top
 NPC_DIR = $(shell pwd)/npc
 CPP_DIR = $(NPC_DIR)/csrc
 VERILOG_DIR = $(NPC_DIR)/vsrc
@@ -17,7 +20,6 @@ VERILOG_SRC = $(wildcard $(VERILOG_DIR)/*.v)
 CPP_SRC = $(wildcard $(CPP_DIR)/*.cpp)
 
 # DO NOT modify the following code!!!
-
 GITFLAGS = -q --author='tracer-ysyx2204 <tracer@ysyx.org>' --no-verify --allow-empty
 
 # prototype: git_commit(msg)
@@ -28,17 +30,34 @@ define git_commit
 	-@sync
 endef
 
+# rules for NVBoard
+include $(NVBOARD_HOME)/scripts/nvboard.mk
+
+INCFLAGS = $(addprefix -I, $(INC_PATH))
+CFLAGS += $(INCFLAGS) -DTOP_NAME="\"V$(TOPNAME)\""
+VERILATOR_CFLAGS += -MMD --build -cc --trace  \
+				-O3 --x-assign fast --x-initial fast --noassert
+LDFLAGS += -lSDL2 -lSDL2_image
+
 sim: build
 	$(call git_commit, "sim RTL") # DO NOT REMOVE THIS LINE!!!
-	@$(BUILD_DIR)/V$(basename $(TOP_MOD));gtkwave $(WAVE_FILE)
+	
+nvboard: build
+	@$(BUILD_DIR)/$(TOPNAME)
+
+gtkwave: build
+	@$(BUILD_DIR)/$(TOPNAME);gtkwave $(WAVE_FILE)
 
 header:
 	echo "" > $(CPP_DIR)/TEMP.h
 	$(foreach f,$(OBJ_SRC), echo '#include"V$(f).h"' >> /$(CPP_DIR)/TEMP.h)
 	echo '#define WAVE_FILE "$(WAVE_FILE)"' >> $(CPP_DIR)/TEMP.h
 
-build: verilog header $(VERILOG_SRC) $(CPP_SRC)
-	verilator -j $(JOB_NUM) --cc --exe --trace --build $(VERILOG_SRC) $(CPP_SRC) --Mdir $(BUILD_DIR)
+build: verilog header $(VERILOG_SRC) $(CPP_SRC) $(NVBOARD_ARCHIVE)
+	verilator $(VERILATOR_CFLAGS) -j $(JOB_NUM) -top $(TOPNAME) \
+				$(VERILOG_SRC) $(CPP_SRC) $(NVBOARD_ARCHIVE) \
+				$(addprefix -CFLAGS , $(CFLAGS)) $(addprefix -LDFLAGS , $(LDFLAGS)) \
+				--Mdir $(BUILD_DIR) -o $(abspath $(BUILD_DIR)/$(TOPNAME))
 
 clean: cleanchisel
 	rm -rf $(BUILD_DIR)
--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -3,43 +3,72 @@
 #include <assert.h>
 #include <verilated.h>          
 #include <verilated_vcd_c.h>
+#include <nvboard.h>
 #include "TEMP.h"
 
-static VGCD* top;
-static VerilatedVcdC* tfp;
-static vluint64_t main_time = 0;
-static const vluint64_t sim_time = 1000;
+static TOP_NAME dut;
 
-int main(int argc, char **argv) {
-  // initialization
-  Verilated::commandArgs(argc, argv);
-  Verilated::traceEverOn(true);
+void nvboard_bind_all_pins(Vtop* top);
 
-	top = new VGCD;
-  tfp = new VerilatedVcdC;
-
-  top->trace(tfp, 99);
-  tfp->open(WAVE_FILE);
-  while (!Verilated::gotFinish() && main_time < sim_time) {
-    if( main_time % 10 == 0 ) top->clock = 0;
-	  if( main_time % 10 == 5 ) top->clock = 1;
-
-    // int a = rand() & 1;
-    // int b = rand() & 1;
-    // top->a = a;
-    // top->b = b;
-    
-    top->eval();
-    tfp->dump(main_time);
-	  main_time++;
-    // assert(top->f == a ^ b);
-  }
-  printf(WAVE_FILE);
-
-  // clean
-  tfp->close();
-  delete top;
-  delete tfp;
-  exit(0);
-  return 0;
+static void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
 }
+
+static void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0) single_cycle();
+  dut.rst = 0;
+}
+
+int main() {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  reset(10);
+
+  while(1) {
+    nvboard_update();
+    dut.clk = !dut.clk;
+    dut.eval();
+  }
+}
+
+// static VGCD* top;
+// static VerilatedVcdC* tfp;
+// static vluint64_t main_time = 0;
+// static const vluint64_t sim_time = 1000;
+
+// int main(int argc, char **argv) {
+//   // initialization
+//   Verilated::commandArgs(argc, argv);
+//   Verilated::traceEverOn(true);
+
+// 	top = new VGCD;
+//   tfp = new VerilatedVcdC;
+
+//   top->trace(tfp, 99);
+//   tfp->open(WAVE_FILE);
+//   while (!Verilated::gotFinish() && main_time < sim_time) {
+//     if( main_time % 10 == 0 ) top->clock = 0;
+// 	  if( main_time % 10 == 5 ) top->clock = 1;
+
+//     // int a = rand() & 1;
+//     // int b = rand() & 1;
+//     // top->a = a;
+//     // top->b = b;
+    
+//     top->eval();
+//     tfp->dump(main_time);
+// 	  main_time++;
+//     // assert(top->f == a ^ b);
+//   }
+//   printf(WAVE_FILE);
+
+//   // clean
+//   tfp->close();
+//   delete top;
+//   delete tfp;
+//   exit(0);
+//   return 0;
+// }
