5d39eb467772c1e546bfefce9fb3d34c72de5018
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-22 23:20:56 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  23:20:56 up  4:32,  1 user,  load average: 1.34, 1.25, 1.19

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,10 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <verilated.h>          
-#include <verilated_vcd_c.h>
 #include <nvboard.h>
-#include "TEMP.h"
+#include <Vtop.h>
 
 static TOP_NAME dut;
 
@@ -33,42 +28,3 @@ int main() {
     dut.eval();
   }
 }
-
-// static VGCD* top;
-// static VerilatedVcdC* tfp;
-// static vluint64_t main_time = 0;
-// static const vluint64_t sim_time = 1000;
-
-// int main(int argc, char **argv) {
-//   // initialization
-//   Verilated::commandArgs(argc, argv);
-//   Verilated::traceEverOn(true);
-
-// 	top = new VGCD;
-//   tfp = new VerilatedVcdC;
-
-//   top->trace(tfp, 99);
-//   tfp->open(WAVE_FILE);
-//   while (!Verilated::gotFinish() && main_time < sim_time) {
-//     if( main_time % 10 == 0 ) top->clock = 0;
-// 	  if( main_time % 10 == 5 ) top->clock = 1;
-
-//     // int a = rand() & 1;
-//     // int b = rand() & 1;
-//     // top->a = a;
-//     // top->b = b;
-    
-//     top->eval();
-//     tfp->dump(main_time);
-// 	  main_time++;
-//     // assert(top->f == a ^ b);
-//   }
-//   printf(WAVE_FILE);
-
-//   // clean
-//   tfp->close();
-//   delete top;
-//   delete tfp;
-//   exit(0);
-//   return 0;
-// }
