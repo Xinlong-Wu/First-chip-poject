238b2e52f80b8c10f95b1c9d23d521de3be21c29
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-22 21:18:53 +0800

    >  generate verilog ysyx_22040000 张三 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  21:18:53 up  2:30,  1 user,  load average: 1.41, 1.14, 1.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char **argv) {
   tfp = new VerilatedVcdC;
 
   top->trace(tfp, 99);
-  tfp->open("../build/res/top.vcd");
+  tfp->open("WAVE_DIR/top.vcd");
   while (!Verilated::gotFinish() && main_time < sim_time) {
     if( main_time % 10 == 0 ) top->clock = 0;
 	  if( main_time % 10 == 5 ) top->clock = 1;
