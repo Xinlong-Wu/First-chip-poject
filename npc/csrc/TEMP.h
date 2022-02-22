b0dae116c74c720bc2443a194e18c2534b39ee03
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-22 23:46:06 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  23:46:06 up  4:58,  1 user,  load average: 0.91, 0.87, 0.84

--- npc/csrc/TEMP.h
+++
@@ -1,3 +0,0 @@
-
-#include"Vled.h" echo #include"Vps2_keyboard.h" echo #include"Vseg.h" echo #include"Vtop.h" echo #include"Vvga_ctrl.h"
-#define WAVE_FILE "/home/vincent/CodeSpace/First-chip-poject/build/top.vcd"
