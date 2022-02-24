31586fafc36cefcfc029bb804adb0974125c0b6d
ysyx_22040200-Wu Xinlong <vincenttttwu@gmail.com>
2022-02-24 16:02:49 +0800

    NJU-ProjectN/am-kernels ics2021 initialized without tracing
    NJU-ProjectN/am-kernels adc316af6e482e6444a9bd68bafc3a57e2cafdbc cpu-tests,Makefile: support target `gdb`

---
+++ npc/chisel/src/mux24.scala
@@ -0,0 +1,14 @@
+import chisel3._
+import chisel3.util.MuxLookup
+
+class mux24 extends Module{
+  val io = IO(new Bundle() {
+    val y = Input(UInt(2.W))
+    val x0 = Input(UInt(2.W))
+    val x1 = Input(UInt(2.W))
+    val x2 = Input(UInt(2.W))
+    val x3 = Input(UInt(2.W))
+    val f = Output(UInt(2.W))
+  })
+  MuxLookup(io.y, 0.U, Array("b00".U -> io.x0, "b01".U -> io.x1, "b10".U -> io.x2, "b11".U -> io.x3 ))
+}
