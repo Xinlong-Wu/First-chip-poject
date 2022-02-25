ab04ae3bef3bed522cbf62f29c2284e169562cf8
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-25 16:46:37 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  16:46:37 up  5:20,  1 user,  load average: 3.45, 3.42, 3.43

--- npc/vsrc/light.v
+++ npc/vsrc/light.v
@@ -1,13 +1,13 @@
 module light(
   input clk,
   input rst,
-  output reg [15:0] led
+  output reg [11:0] led
 );
   reg [31:0] count;
   always @(posedge clk) begin
     if (rst) begin led <= 1; count <= 0; end
     else begin
-      if (count == 0) led <= {led[14:0], led[15]};
+      if (count == 0) led <= {led[10:0], led[11]};
       count <= (count >= 50000 ? 32'b0 : count + 1);
     end
   end
