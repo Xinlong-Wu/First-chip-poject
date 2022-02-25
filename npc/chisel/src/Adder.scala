da53c604adb440a43cc74ec77b537d1287af63d5
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-25 17:26:26 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  17:26:26 up  6:00,  1 user,  load average: 2.35, 2.57, 2.89

---
+++ npc/chisel/src/Adder.scala
@@ -0,0 +1,42 @@
+import chisel3._
+
+class FullAdder extends Module{
+  val io = IO(new Bundle() {
+    val in1 = Input(UInt(1.W))
+    val in2 = Input(UInt(1.W))
+    val cin = Input(UInt(1.W))
+    val out = Output(UInt(1.W))
+    val overflow = Output(UInt(1.W))
+  })
+
+  val in1_xor_in2 = io.in1 ^ io.in2
+  io.out := in1_xor_in2 ^ io.cin
+
+  val in1_and_in2 = io.in1 & io.in2
+  io.overflow := (in1_xor_in2 & io.cin) | in1_and_in2
+}
+
+class Adder(bitWidth: Int) extends Module{
+  val io = IO(new Bundle() {
+    val in1 = Input(UInt(bitWidth.W))
+    val in2 = Input(UInt(bitWidth.W))
+    val cin = Input(UInt(1.W))
+    val out = Output(UInt(bitWidth.W))
+    val overflow = Output(UInt(1.W))
+  })
+  val cinVec = Wire(Vec(bitWidth+1,UInt(1.W)))
+  val res = Wire(Vec(bitWidth,UInt(1.W)))
+  val FAs = Array.fill(bitWidth)(Module(new FullAdder()).io)
+  cinVec(0) := io.cin
+
+  for(i <- 0 until bitWidth){
+    FAs(i).in1 := io.in1(i)
+    FAs(i).in2 := io.in2(i)
+    FAs(i).cin := cinVec(i)
+    res(i) := FAs(i).out
+    cinVec(i+1) := FAs(i).overflow
+  }
+
+  io.overflow := cinVec(bitWidth)
+  io.out := res.asUInt
+}
