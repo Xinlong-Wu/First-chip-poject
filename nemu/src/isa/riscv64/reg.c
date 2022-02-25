b11ab1f615e7961dcdfa5427001c53c8d2ff41d9
ysyx_22040200-Wu Xinlong <vincenttttwu@gmail.com>
2022-02-25 13:14:41 +0800

    update PA

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -8,9 +8,57 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
-void isa_reg_display() {
+extern riscv64_CPU_state cpu;
+
+void isa_reg_display(char* reg) {
+
+  char *cmd = strtok(reg, " ");
+  if (cmd == NULL){
+    printf("Reg: ");
+    for (int i = 0; i < 32; i++){
+      printf("%s: %lu\t",regs[i], cpu.gpr[i]);
+      if(i % 8 == 0)
+        printf("\n");
+    }
+    printf("\n");
+    return;
+  }
+
+  bool isSuccess = false;
+  word_t res = isa_reg_str2val(cmd, &isSuccess);
+
+  if(isSuccess)
+    printf("Reg %s: %lu\n", cmd, res);
+  else
+    printf("%s\n", ASNI_FMT(str(Error: wrong register.), ASNI_FG_RED));
+
+  return;
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+
+  *success = false;
+
+  if(strcmp(s, "zero") == 0){
+    *success = true;
+    return cpu.gpr[0];
+  }
+
+  if(*s == 'x'){
+    int index = atoi(s+1);
+    if (index < 32){
+      *success = true;
+      return cpu.gpr[index];
+    }
+    return 0;
+  }
+
+  for (int i = 0;i < 32; i++){
+    if(strcmp(s, regs[i]) == 0){
+      *success = true;
+      return cpu.gpr[i];
+    }
+  }
+  
   return 0;
 }
--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -1,5 +1,7 @@
 #include <isa.h>
+#include <memory/paddr.h>
 #include <cpu/cpu.h>
+#include <cpu/decode.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
@@ -37,6 +39,73 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_info(char *args) {
+  Log("cmd_info get arg %s", args);
+  char *cmd = strtok(args, " ");
+  args = cmd + strlen(cmd) +1;
+
+  if (strcmp(cmd,"r") == 0){
+    isa_reg_display(args);
+  }
+  else if (strcmp(cmd, "w") == 0){
+    Log("TBD: Show WatchPoint %s",args);
+  }
+
+  return 0;
+}
+
+extern word_t vaddr_read(vaddr_t addr, int len);
+
+static int cmd_x(char *args) {
+
+  int len = 0;
+  paddr_t addr = 0;
+  if(args)
+    sscanf(args,"%d %x",&len,&addr);
+
+  if(len != 0 && addr!=0){
+    int onceLength = sizeof(word_t) < len ? sizeof(word_t) : (len>>1)<<1;
+    if (onceLength == 0)
+      onceLength = 1;
+    
+    if(!in_pmem(addr))
+      addr += CONFIG_MBASE;
+    if(in_pmem(addr)){
+      int printCount = 0;
+      printf("0x%x:\t", addr);
+      for(int i = len;i > 0; i-=onceLength){
+        word_t data = vaddr_read(addr, onceLength);
+        printf("0x%016lx\t",data);
+        if (printCount++ % 4 == 0)
+          printf("\n\t");
+        addr+=onceLength;
+        while (onceLength > i)
+          onceLength/=2;
+      }
+    }
+    else
+      printf("%s\n", ASNI_FMT(str(Error: valid mem address.), ASNI_FG_RED));
+    return 0;
+  }
+  printf("%s\n", ASNI_FMT(str(Error: valid mem address.), ASNI_FG_RED));
+  return 0;
+}
+
+static int cmd_si(char *args){
+  int n = 1;
+  if(args)
+    sscanf(args,"%x",&n);
+  
+  Decode s;
+  s.pc = cpu.pc;
+  s.snpc = cpu.pc;
+  while (n--){
+    isa_exec_once(&s);
+    cpu.pc = s.dnpc;
+  }
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -49,7 +118,12 @@ static struct {
   { "q", "Exit NEMU", cmd_q },
 
   /* TODO: Add more commands */
-
+  {"si", "si [N], step in the program", cmd_si},
+  {"info", "info [r|w], out put the info of Regesiter or WatchPoint", cmd_info},
+  {"x", "x [N] [EXPR] , out put N Bite data from value of EXPR by sixteen format", cmd_x},
+  // {"p", "p [EXPR], out put the value of EXPR"},
+  // {"w", "w [EXPR], set WatchPoint stop the program if the value of EXPR has changed"},
+  // {"d", "d [N], delete WatchPoint witch id is N"},
 };
 
 #define NR_CMD ARRLEN(cmd_table)
@@ -82,6 +156,7 @@ void sdb_set_batch_mode() {
 }
 
 void sdb_mainloop() {
+  Log("SDB Mod");
   if (is_batch_mode) {
     cmd_c(NULL);
     return;
