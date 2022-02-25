9dbb57f52ce5181117f4e02bf73463bdb24edf4f
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-25 16:55:40 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  16:55:40 up  5:29,  1 user,  load average: 3.40, 3.41, 3.41

--- npc/chisel/src/bcd7seg.scala
+++ npc/chisel/src/bcd7seg.scala
@@ -11,27 +11,27 @@ class bcd7seg extends Module{
   encode := "b11111111".U
   io.HEX := "b11111111".U
 
-  when(io.en){
-    when(io.num === 0.U){
-      io.HEX := Cat(encode(7, 6), 0.U(6.W))
-    }.elsewhen(io.num === 1.U){
-      io.HEX := Cat(encode(7, 3), 0.U(2.W), encode(0))
-    }.elsewhen(io.num === 2.U){
-      io.HEX := Cat(encode(7), 0.U(1.W), encode(5), 0.U(2.W), encode(2), 0.U(2.W))
-    }.elsewhen(io.num === 3.U){
-      io.HEX := Cat(encode(7), 0.U(1.W), encode(5, 4), 0.U(4.W))
-    }.elsewhen(io.num === 4.U){
-      io.HEX := Cat(encode(7), 0.U(2.W), encode(4, 3), 0.U(2.W), encode(0))
-    }.elsewhen(io.num === 5.U){
-      io.HEX := Cat(encode(7), 0.U(2.W), encode(4), 0.U(2.W), encode(1), 0.U(1.W))
-    }.elsewhen(io.num === 6.U){
-      io.HEX := Cat(encode(7), 0.U(5.W), encode(1), 0.U(1.W))
-    }.elsewhen(io.num === 7.U){
-      io.HEX := Cat(encode(7, 3), 0.U(3.W))
-    }.elsewhen(io.num === 8.U){
-      io.HEX := Cat(encode(7, 6), 0.U(6.W))
-    }.elsewhen(io.num === 9.U){
-      io.HEX := Cat(encode(7), 0.U(2.W), encode(4), 0.U(4.W))
-    }
-  }
+//  when(io.en){
+//    when(io.num === 0.U){
+//      io.HEX := Cat(encode(7, 6), 0.U(6.W))
+//    }.elsewhen(io.num === 1.U){
+//      io.HEX := Cat(encode(7, 3), 0.U(2.W), encode(0))
+//    }.elsewhen(io.num === 2.U){
+//      io.HEX := Cat(encode(7), 0.U(1.W), encode(5), 0.U(2.W), encode(2), 0.U(2.W))
+//    }.elsewhen(io.num === 3.U){
+//      io.HEX := Cat(encode(7), 0.U(1.W), encode(5, 4), 0.U(4.W))
+//    }.elsewhen(io.num === 4.U){
+//      io.HEX := Cat(encode(7), 0.U(2.W), encode(4, 3), 0.U(2.W), encode(0))
+//    }.elsewhen(io.num === 5.U){
+//      io.HEX := Cat(encode(7), 0.U(2.W), encode(4), 0.U(2.W), encode(1), 0.U(1.W))
+//    }.elsewhen(io.num === 6.U){
+//      io.HEX := Cat(encode(7), 0.U(5.W), encode(1), 0.U(1.W))
+//    }.elsewhen(io.num === 7.U){
+//      io.HEX := Cat(encode(7, 3), 0.U(3.W))
+//    }.elsewhen(io.num === 8.U){
+//      io.HEX := Cat(encode(7, 6), 0.U(6.W))
+//    }.elsewhen(io.num === 9.U){
+//      io.HEX := Cat(encode(7), 0.U(2.W), encode(4), 0.U(4.W))
+//    }
+//  }
 }
