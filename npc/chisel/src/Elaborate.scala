7c3a65395215325d73767863a1f982c3981b6b15
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-25 22:03:02 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  22:03:02 up 10:36,  1 user,  load average: 2.25, 1.30, 1.00

--- npc/chisel/src/Elaborate.scala
+++ npc/chisel/src/Elaborate.scala
@@ -3,8 +3,6 @@ object Elaborate extends App {
   (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new bcd7seg())))
   (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new encoder83())))
   (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new RippleCarryAdder(8))))
-  (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new CarryLookAheadAdder(8))))
-  (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new Arithmetic(8))))
   (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new ALU(8))))
   (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new Counter(8))))
 }
