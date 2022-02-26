c48eb5285eb834aee38d12157694794a819be072
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-26 11:26:54 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  11:26:54 up 39 min,  1 user,  load average: 2.52, 1.87, 1.46

--- npc/chisel/src/chiseltop.scala
+++ npc/chisel/src/chiseltop.scala
@@ -3,7 +3,7 @@ import chisel3._
 
 class chiseltop extends RawModule{
   val io = IO(new Bundle {
-    val clock = Input(UInt(1.W))
+    val clock = Input(Clock())
     val reset = Input(UInt(1.W))
     val sw = Input(UInt(16.W))
     val ps2_clk = Input(UInt(1.W))
@@ -44,7 +44,39 @@ class chiseltop extends RawModule{
   io.VGA_B := 0.U(8.W)
 
   var flowlight = Module(new light())
-  flowlight.io.clk := io.clock
+  flowlight.io.clk := io.clock.asUInt
   flowlight.io.rst := io.reset
   io.ledr := Cat(flowlight.io.led, 0.U(3.W))
+
+  withClockAndReset(io.clock, io.reset.asBool){
+    val clk_1s = Reg(UInt(1.W))
+    clk_1s := 0.U
+    val count_clk = Reg(UInt(25.W))
+    when(count_clk === 24999999.U){
+      count_clk := 0.U
+      clk_1s := ~clk_1s
+    }.otherwise{
+      count_clk := count_clk + 1.U
+    }
+
+    val counter = Module(new Counter(10))
+    counter.io.en := 1.U
+
+    val num1 = Module(new bcd7seg())
+    num1.io.en := 1.U
+    num1.io.num := (counter.io.out % 10.U(4.W))(3, 0)
+    io.seg0 := num1.io.HEX
+
+    val num2 = Module(new bcd7seg())
+    num2.io.en := 1.U
+    num2.io.num := ((counter.io.out % 100.U)/10.U)(3, 0)
+    io.seg1 := num2.io.HEX
+
+    val num3 = Module(new bcd7seg())
+    num3.io.en := 1.U
+    num3.io.num := ((counter.io.out % 100.U)/10.U)(3, 0)
+    io.seg2 := num3.io.HEX
+  }
+
+
 }
