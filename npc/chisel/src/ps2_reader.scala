31f7befbba9a4fd3cbe7be4074824dacec279d9d
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-26 21:52:48 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  21:52:48 up 11:05,  1 user,  load average: 1.89, 1.56, 1.43

--- npc/chisel/src/ps2_reader.scala
+++ npc/chisel/src/ps2_reader.scala
@@ -2,17 +2,21 @@ import chisel3._
 
 class ps2_reader extends Module {
   val io = IO(new Bundle() {
+    val readClk = Input(Clock())
     val data = Input(UInt(8.W))
     val en = Input(Bool())
     val finish = Output(Bool())
 //    val outdata = Output(UInt(8.W))
   })
-  io.finish := ~io.en
+//  io.finish := ~io.en
+  withClock(io.readClk){
+    val ps2data = Wire(UInt(8.W))
+    ps2data := io.data
 
-  val ps2data = Wire(UInt(8.W))
-  ps2data := io.data
-
-  when(clock.asBool){
-    printf(p"got $io \n")
+    when(clock.asBool){
+      printf(p"got $io \n")
+    }
   }
+
+  when()
 }
