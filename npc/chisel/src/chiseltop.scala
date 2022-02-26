56881c1218c100992458f338230bf7e175aaabd6
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-26 16:36:00 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  16:36:00 up  5:48,  1 user,  load average: 2.89, 1.56, 1.41

--- npc/chisel/src/chiseltop.scala
+++ npc/chisel/src/chiseltop.scala
@@ -53,8 +53,6 @@ class chiseltop extends RawModule{
 
   withClockAndReset(clock, reset.asBool){
     val flowlight = Module(new light())
-    flowlight.io.clk := clock
-    flowlight.io.rst := reset
     io.ledr := Cat(flowlight.io.led, 0.U(3.W))
 
     val (_, clk_1s) = Counter(true.B, 24999999)
