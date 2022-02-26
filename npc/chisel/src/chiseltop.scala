5cb7fbf53644a89d240c80f3647c14f919ecf58f
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-26 12:21:42 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  12:21:42 up  1:34,  1 user,  load average: 1.57, 1.58, 1.53

--- npc/chisel/src/chiseltop.scala
+++ npc/chisel/src/chiseltop.scala
@@ -2,10 +2,8 @@ import Chisel.Cat
 import chisel3._
 import chisel3.util.Counter
 
-class chiseltop extends RawModule{
+class chiseltop extends Module{
   val io = IO(new Bundle {
-    val clock = Input(Clock())
-    val reset = Input(UInt(1.W))
     val sw = Input(UInt(16.W))
     val ps2_clk = Input(UInt(1.W))
     val ps2_data = Input(UInt(1.W))
@@ -45,30 +43,26 @@ class chiseltop extends RawModule{
   io.VGA_B := 0.U(8.W)
 
   val flowlight = Module(new light())
-  flowlight.io.clk := io.clock.asUInt
-  flowlight.io.rst := io.reset
   io.ledr := Cat(flowlight.io.led, 0.U(3.W))
 
-  withClockAndReset(io.clock, io.reset.asBool){
-    val (_, clk_1s) = Counter(true.B, 24999999)
+  val (_, clk_1s) = Counter(true.B, 24999999)
 
-    withClock(clk_1s.asClock){
-      val (counterRes, _) = Counter(true.B, 233)
+  withClock(clk_1s.asClock){
+    val (counterRes, _) = Counter(true.B, 233)
 
-      val num1 = Module(new bcd7seg())
-      num1.io.en := 1.U
-      num1.io.num := (counterRes % 10.U(8.W))(3, 0)
-      io.seg0 := num1.io.HEX
+    val num1 = Module(new bcd7seg())
+    num1.io.en := 1.U
+    num1.io.num := (counterRes % 10.U(8.W))(3, 0)
+    io.seg0 := num1.io.HEX
 
-      val num2 = Module(new bcd7seg())
-      num2.io.en := 1.U
-      num2.io.num := ((counterRes % 100.U(8.W))/10.U(8.W))(3, 0)
-      io.seg1 := num2.io.HEX
+    val num2 = Module(new bcd7seg())
+    num2.io.en := 1.U
+    num2.io.num := ((counterRes % 100.U(8.W))/10.U(8.W))(3, 0)
+    io.seg1 := num2.io.HEX
 
-      val num3 = Module(new bcd7seg())
-      num3.io.en := 1.U
-      num3.io.num := (counterRes / 100.U(8.W))(3, 0)
-      io.seg2 := num3.io.HEX
-    }
+    val num3 = Module(new bcd7seg())
+    num3.io.en := 1.U
+    num3.io.num := (counterRes / 100.U(8.W))(3, 0)
+    io.seg2 := num3.io.HEX
   }
 }
