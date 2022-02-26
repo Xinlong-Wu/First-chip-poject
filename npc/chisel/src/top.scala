2aff521ec019b717679b83459e253b6c0049155f
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-26 16:00:26 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  16:00:26 up  5:13,  1 user,  load average: 3.45, 2.60, 2.03

--- npc/chisel/src/top.scala
+++ npc/chisel/src/top.scala
@@ -6,47 +6,49 @@ import chisel3.util.experimental.loadMemoryFromFile
 class top extends RawModule{
   val clock = IO(Input(Clock()))
   val reset = IO(Input(UInt(1.W)))
-  val sw = IO(Input(UInt(16.W)))
-  val ps2_clk = IO(Input(UInt(1.W)))
-  val ps2_data = IO(Input(UInt(1.W)))
-  val ledr = IO(Output(UInt(16.W)))
-  val VGA_CLK = IO(Output(UInt(1.W)))
-  val VGA_HSYNC = IO(Output(UInt(1.W)))
-  val VGA_VSYNC = IO(Output(UInt(1.W)))
-  val VGA_BLANK_N = IO(Output(UInt(1.W)))
-  val VGA_R = IO(Output(UInt(8.W)))
-  val VGA_G = IO(Output(UInt(8.W)))
-  val VGA_B = IO(Output(UInt(8.W)))
-  val seg0 = IO(Output(UInt(8.W)))
-  val seg1 = IO(Output(UInt(8.W)))
-  val seg2 = IO(Output(UInt(8.W)))
-  val seg3 = IO(Output(UInt(8.W)))
-  val seg4 = IO(Output(UInt(8.W)))
-  val seg5 = IO(Output(UInt(8.W)))
-  val seg6 = IO(Output(UInt(8.W)))
-  val seg7 = IO(Output(UInt(8.W)))
+  val io = IO(new Bundle {
+    val sw = Input(UInt(16.W))
+    val ps2_clk = Input(UInt(1.W))
+    val ps2_data = Input(UInt(1.W))
+    val ledr = Output(UInt(16.W))
+    val VGA_CLK = Output(UInt(1.W))
+    val VGA_HSYNC = Output(UInt(1.W))
+    val VGA_VSYNC = Output(UInt(1.W))
+    val VGA_BLANK_N = Output(UInt(1.W))
+    val VGA_R = Output(UInt(8.W))
+    val VGA_G = Output(UInt(8.W))
+    val VGA_B = Output(UInt(8.W))
+    val seg0 = Output(UInt(8.W))
+    val seg1 = Output(UInt(8.W))
+    val seg2 = Output(UInt(8.W))
+    val seg3 = Output(UInt(8.W))
+    val seg4 = Output(UInt(8.W))
+    val seg5 = Output(UInt(8.W))
+    val seg6 = Output(UInt(8.W))
+    val seg7 = Output(UInt(8.W))
+  })
 
-  seg0 := "b11111111".U
-  seg1 := "b11111111".U
-  seg2 := "b11111111".U
-  seg3 := "b11111111".U
-  seg4 := "b11111111".U
-  seg5 := "b11111111".U
-  seg6 := "b11111111".U
-  seg7 := "b11111111".U
-  ledr := 0.U(16.W)
-  VGA_CLK := 0.U(1.W)
-  VGA_HSYNC := 0.U(1.W)
-  VGA_VSYNC := 0.U(1.W)
-  VGA_BLANK_N := 0.U(1.W)
-  VGA_R := 0.U(8.W)
-  VGA_G := 0.U(8.W)
-  VGA_B := 0.U(8.W)
+  io.seg0 := "b11111111".U
+  io.seg1 := "b11111111".U
+  io.seg2 := "b11111111".U
+  io.seg3 := "b11111111".U
+  io.seg4 := "b11111111".U
+  io.seg5 := "b11111111".U
+  io.seg6 := "b11111111".U
+  io.seg7 := "b11111111".U
+  io.ledr := 0.U(16.W)
+  io.VGA_CLK := 0.U(1.W)
+  io.VGA_HSYNC := 0.U(1.W)
+  io.VGA_VSYNC := 0.U(1.W)
+  io.VGA_BLANK_N := 0.U(1.W)
+  io.VGA_R := 0.U(8.W)
+  io.VGA_G := 0.U(8.W)
+  io.VGA_B := 0.U(8.W)
 
   val flowlight = Module(new light())
   flowlight.io.clk := clock
   flowlight.io.rst := reset
-  ledr := Cat(flowlight.io.led, 0.U(3.W))
+  io.ledr := Cat(flowlight.io.led, 0.U(3.W))
 
   val h_addr = Wire(UInt(10.W))
   val v_addr = Wire(UInt(10.W))
@@ -64,23 +66,23 @@ class top extends RawModule{
     val num1 = Module(new bcd7seg())
     num1.io.en := 1.U
     num1.io.num := (counterRes % 10.U(8.W))(3, 0)
-    seg0 := num1.io.HEX
+    io.seg0 := num1.io.HEX
 
     val num2 = Module(new bcd7seg())
     num2.io.en := 1.U
     num2.io.num := ((counterRes % 100.U(8.W))/10.U(8.W))(3, 0)
-    seg1 := num2.io.HEX
+    io.seg1 := num2.io.HEX
 
     val num3 = Module(new bcd7seg())
     num3.io.en := 1.U
     num3.io.num := (counterRes / 100.U(8.W))(3, 0)
-    seg2 := num3.io.HEX
+    io.seg2 := num3.io.HEX
 
     val my_keyboard = Module(new ps2_keyboard())
     my_keyboard.io.clk := clock.asUInt
     my_keyboard.io.resetn := ~reset
-    my_keyboard.io.ps2_clk := ps2_clk
-    my_keyboard.io.ps2_data := ps2_data
+    my_keyboard.io.ps2_clk := io.ps2_clk
+    my_keyboard.io.ps2_data := io.ps2_data
 
     val my_vga_ctrl = Module(new vga_ctrl())
     my_vga_ctrl.io.pclk := clock.asUInt
@@ -88,12 +90,12 @@ class top extends RawModule{
     my_vga_ctrl.io.vga_data := vga_data
     h_addr := my_vga_ctrl.io.h_addr
     v_addr := my_vga_ctrl.io.v_addr
-    VGA_HSYNC := my_vga_ctrl.io.hsync
-    VGA_VSYNC := my_vga_ctrl.io.vsync
-    VGA_BLANK_N := my_vga_ctrl.io.valid
-    VGA_R := my_vga_ctrl.io.vga_r
-    VGA_G := my_vga_ctrl.io.vga_g
-    VGA_B := my_vga_ctrl.io.vga_b
+    io.VGA_HSYNC := my_vga_ctrl.io.hsync
+    io.VGA_VSYNC := my_vga_ctrl.io.vsync
+    io.VGA_BLANK_N := my_vga_ctrl.io.valid
+    io.VGA_R := my_vga_ctrl.io.vga_r
+    io.VGA_G := my_vga_ctrl.io.vga_g
+    io.VGA_B := my_vga_ctrl.io.vga_b
   }
 
   val mem = Module(new vmem())
