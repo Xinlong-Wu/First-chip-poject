c55b3f8f52819fc496df76b3105911203e2611e3
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-27 16:52:12 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  16:52:12 up  5:32,  1 user,  load average: 2.61, 1.65, 1.42

--- npc/chisel/src/top.scala
+++ npc/chisel/src/top.scala
@@ -104,12 +104,18 @@ class top extends Module{
   VGA_G := my_vga_ctrl.io.vga_g
   VGA_B := my_vga_ctrl.io.vga_b
 
-
-  val mem = Module(new vmem(524288,24, "npc/resource/picture.hex"))
+  val mem = Module(new vmem(524288,24, "npc/resource/AsciiMask.hex"))
   mem.clock := clock
   mem.reset := reset
   mem.io.h_addr := h_addr
   mem.io.v_addr := v_addr(8,0)
   vga_data := mem.io.vga_data
+
+//  val mem = Module(new vmem(524288,24, "npc/resource/picture.hex"))
+//  mem.clock := clock
+//  mem.reset := reset
+//  mem.io.h_addr := h_addr
+//  mem.io.v_addr := v_addr(8,0)
+//  vga_data := mem.io.vga_data
 
 }
