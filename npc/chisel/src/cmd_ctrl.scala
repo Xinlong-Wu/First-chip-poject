a0976f344e105c9a8c486260535e929c01c53ccc
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-01 20:33:28 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  20:33:28 up  3:05,  1 user,  load average: 2.87, 2.07, 1.66

--- npc/chisel/src/cmd_ctrl.scala
+++ npc/chisel/src/cmd_ctrl.scala
@@ -45,7 +45,7 @@ class cmd_ctrl extends Module {
   var data_offset = Wire(UInt(8.W))
   data_offset := 31.U - ((io.v_addr(1,0) * 8.U) + io.h_addr(2,0))
 
-  printf(p"index $index, ch_index $ch_index, block_index $block_index, ch_data $ch_data, data_offset $data_offset, io:$io\n")
+//  printf(p"index $index, ch_index $ch_index, block_index $block_index, ch_data $ch_data, data_offset $data_offset, io:$io\n")
 
   io.data := Fill(24, ch_data(data_offset))
 //  io.data := Fill(24, 1.U)
