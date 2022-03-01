d159487e9c6c2b153f95ce939a59b13641eec4cf
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-01 19:59:59 +0800

    >  generate verilog ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  19:59:59 up  2:32,  1 user,  load average: 1.56, 0.87, 0.64

--- npc/chisel/test/src/cmdCtrlSpec.scala
+++ npc/chisel/test/src/cmdCtrlSpec.scala
@@ -4,14 +4,14 @@ import org.scalatest.freespec.AnyFreeSpec
 
 class cmdCtrlSpec extends AnyFreeSpec with ChiselScalatestTester {
   "test " in {
-    test(new cmd_ctrl()) { c =>
-      for(v_addr <- 0 until 480){
-        for(h_addr <- 0 until 640){
-          c.io.h_addr.poke(h_addr.U)
-          c.io.v_addr.poke(h_addr.U)
-          c.clock.step(1)
-        }
-      }
-    }
+//    test(new cmd_ctrl()) { c =>
+//      for(v_addr <- 0 until 480){
+//        for(h_addr <- 0 until 640){
+//          c.io.h_addr.poke(h_addr.U)
+//          c.io.v_addr.poke(h_addr.U)
+//          c.clock.step(1)
+//        }
+//      }
+//    }
   }
 }
