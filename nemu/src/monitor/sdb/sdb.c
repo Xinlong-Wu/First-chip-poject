c018bdf33ed42320fdddf9fa17717ca282094a16
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-06 11:54:39 +0800

    >  compile NEMU ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  11:54:39 up  1:10,  1 user,  load average: 0.61, 0.57, 0.33

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -68,7 +68,7 @@ static int cmd_x(char *args) {
   paddr_t addr = 0;
   if(args)
     len = atoi(strtok(args, " "));
-  Log("arg: %s", args);
+  Log("arg: %s", args+1);
 
   if(len != 0 && addr!=0){
     int onceLength = sizeof(word_t) < len ? sizeof(word_t) : (len>>1)<<1;
