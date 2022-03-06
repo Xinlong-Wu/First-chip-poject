2901a98eafbb8fa3a6da539a5fcd3b0d06ce1419
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-06 17:37:15 +0800

    >  compile NEMU ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  17:37:15 up  6:52,  1 user,  load average: 4.13, 3.22, 3.20

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -5,7 +5,7 @@ const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6", "pc",
 };
 
 extern riscv64_CPU_state cpu;
@@ -36,6 +36,7 @@ void isa_reg_display(char* reg) {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  assert(s != NULL && success != NULL);
 
   *success = false;
 
@@ -53,6 +54,11 @@ word_t isa_reg_str2val(const char *s, bool *success) {
     return 0;
   }
 
+  if (strcmp(s, "pc")){
+    *success = true;
+    return cpu.pc;
+  }
+  
   for (int i = 0;i < 32; i++){
     if(strcmp(s, regs[i]) == 0){
       *success = true;
