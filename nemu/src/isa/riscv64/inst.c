9ad066ac4514eed7031e86303f6a672f2da06c7b
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 23:30:56 +0800

    >  compile NEMU ysyx_22040200 乌鑫龙 Linux Vincent-PC 5.16.10-arch1-1 #1 SMP PREEMPT Wed, 16 Feb 2022 19:35:18 +0000 x86_64 GNU/Linux  23:30:56 up  1:59,  1 user,  load average: 1.87, 1.69, 1.66

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -8,7 +8,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_R,
+  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_R, TYPE_B,
   TYPE_N, // none
 };
 
@@ -23,6 +23,7 @@ static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
 static word_t immJ(uint32_t i) { return SEXT((BITS(i, 31, 31)<<20)|(BITS(i, 30, 21)<<1)|(BITS(i, 20, 20)<<11)|(BITS(i, 19, 12)<<12), 20); }
+static word_t immB(uint32_t i) { return SEXT((BITS(i, 31, 31)<<12)|(BITS(i, 30, 25)<<5)|(BITS(i, 11, 8)<<1)|(BITS(i, 7, 7)<<11), 13); }
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
@@ -31,6 +32,7 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
   int rs2 = BITS(i, 24, 20);
   destR(rd);
   switch (type) {
+    case TYPE_B: destI(immB(i)); src1R(rs1); src2R(rs2); break;
     case TYPE_I: src1I(rs1);     src2I(immI(i)); break;
     case TYPE_U: src1I(immU(i)); break;
     case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
@@ -50,7 +52,8 @@ static int decode_exec(Decode *s) {
 }
 
   INSTPAT_START();
-  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc   , U, R(dest) = src1 + s->pc);
+
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq     , I, if(R(src1) == R(src2)) s->dnpc = s->pc + dest);
 
   INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld      , I, R(dest) = Mr(R(src1) + src2, 8));
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi    , I, R(dest) = R(src1) + src2);
@@ -58,15 +61,17 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr    , I, s->dnpc = (R(src1) + SEXT(src2, 12))&(~1); R(dest) = s->pc + 4);
 
   INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal     , J, s->dnpc = s->pc + src1; R(dest) = s->pc + 4);
-
+  
   INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add     , R, R(dest) = R(src1) + R(src2));
   INSTPAT("0100000 ????? ????? 000 ????? 01100 11", add     , R, R(dest) = R(src1) - R(src2));
 
+  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc   , U, R(dest) = src1 + s->pc);
+
   INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd      , S, Mw(src1 + dest, 8, src2));
   
-
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak  , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv     , N, INV(s->pc));
+
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
