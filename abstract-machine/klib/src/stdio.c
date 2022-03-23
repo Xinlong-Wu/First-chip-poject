c6718016b877ea0fba18f8ded4d9a6448a473dbd
ysyx_22040200-Wu Xinlong <vincenttttwu@gmail.com>
2022-03-24 00:07:54 +0800

    pass hello-src.c

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,11 +10,110 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int format = 0;
+	int longarg = 0;
+	size_t pos = 0;
+	for (; *fmt; fmt++) {
+		if (format) {
+			switch(*fmt) {
+			case 'l': {
+				longarg = 1;
+				break;
+			}
+			case 'p': {
+				longarg = 1;
+				if (out) {
+					out[pos] = '0';
+				}
+				pos++;
+				if (out) {
+					out[pos] = 'x';
+				}
+				pos++;
+			}
+			case 'x': {
+				long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
+				int hexdigits = 2*(longarg ? sizeof(long) : sizeof(int))-1;
+				for(int i = hexdigits; i >= 0; i--) {
+					int d = (num >> (4*i)) & 0xF;
+					if (out) {
+						out[pos] = (d < 10 ? '0'+d : 'a'+d-10);
+					}
+					pos++;
+				}
+				longarg = 0;
+				format = 0;
+				break;
+			}
+			case 'd': {
+				long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
+				if (num < 0) {
+					num = -num;
+					if (out) {
+						out[pos] = '-';
+					}
+					pos++;
+				}
+				long digits = 1;
+				for (long nn = num; nn /= 10; digits++);
+				for (int i = digits-1; i >= 0; i--) {
+					if (out) {
+						out[pos + i] = '0' + (num % 10);
+					}
+					num /= 10;
+				}
+				pos += digits;
+				longarg = 0;
+				format = 0;
+				break;
+			}
+			case 's': {
+				const char * s2 = va_arg(ap, const char *);
+				while (*s2) {
+					if (out) {
+						out[pos] = *s2;
+					}
+					pos++;
+					s2++;
+				}
+				longarg = 0;
+				format = 0;
+				break;
+			}
+			case 'c': {
+				if (out) {
+					out[pos] = (char)va_arg(ap,int);
+				}
+				pos++;
+				longarg = 0;
+				format = 0;
+				break;
+			}
+			default:
+				break;
+			}
+		} else if (*fmt == '%') {
+			format = 1;
+		} else {
+			if (out) {
+				out[pos] = *fmt;
+			}
+			pos++;
+		}
+    	}
+	if (out) {
+		out[pos] = 0;
+	}
+	return pos;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  int res = 0;
+	va_list vl;
+	va_start(vl, fmt);
+	res = vsprintf(out, fmt, vl);
+	va_end(vl);
+	return res;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
@@ -23,6 +122,103 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   panic("Not implemented");
+  int format = 0;
+	int longarg = 0;
+	size_t pos = 0;
+	for (; *fmt; fmt++) {
+		if (format) {
+			switch(*fmt) {
+			case 'l': {
+				longarg = 1;
+				break;
+			}
+			case 'p': {
+				longarg = 1;
+				if (out && pos < n) {
+					out[pos] = '0';
+				}
+				pos++;
+				if (out && pos < n) {
+					out[pos] = 'x';
+				}
+				pos++;
+			}
+			case 'x': {
+				long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
+				int hexdigits = 2*(longarg ? sizeof(long) : sizeof(int))-1;
+				for(int i = hexdigits; i >= 0; i--) {
+					int d = (num >> (4*i)) & 0xF;
+					if (out && pos < n) {
+						out[pos] = (d < 10 ? '0'+d : 'a'+d-10);
+					}
+					pos++;
+				}
+				longarg = 0;
+				format = 0;
+				break;
+			}
+			case 'd': {
+				long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
+				if (num < 0) {
+					num = -num;
+					if (out && pos < n) {
+						out[pos] = '-';
+					}
+					pos++;
+				}
+				long digits = 1;
+				for (long nn = num; nn /= 10; digits++);
+				for (int i = digits-1; i >= 0; i--) {
+					if (out && pos + i < n) {
+						out[pos + i] = '0' + (num % 10);
+					}
+					num /= 10;
+				}
+				pos += digits;
+				longarg = 0;
+				format = 0;
+				break;
+			}
+			case 's': {
+				const char * s2 = va_arg(ap, const char *);
+				while (*s2) {
+					if (out && pos < n) {
+						out[pos] = *s2;
+					}
+					pos++;
+					s2++;
+				}
+				longarg = 0;
+				format = 0;
+				break;
+			}
+			case 'c': {
+				if (out && pos < n) {
+					out[pos] = (char)va_arg(ap,int);
+				}
+				pos++;
+				longarg = 0;
+				format = 0;
+				break;
+			}
+			default:
+				break;
+			}
+		} else if (*fmt == '%') {
+			format = 1;
+		} else {
+			if (out && pos < n) {
+				out[pos] = *fmt;
+			}
+			pos++;
+		}
+    	}
+	if (out && pos < n) {
+		out[pos] = 0;
+	} else if (out && n) {
+		out[n-1] = 0;
+	}
+	return pos;
 }
 
 #endif
