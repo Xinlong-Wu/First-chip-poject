99ede0117adcd1b009e1ab130d7c9905baef796a
ysyx_22040200-Wu Xinlong <vincenttttwu@gmail.com>
2022-03-25 16:43:24 +0800

    trace mem

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -43,14 +43,38 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+#ifdef CONFIG_TRACE_MEM
+  log_write("Read Memory: *0x%x ", addr);
+#endif
+  if (likely(in_pmem(addr))) {
+    word_t data = pmem_read(addr, len);
+#ifdef CONFIG_TRACE_MEM
+  log_write("=> [ 0x%08lx ], length = %d\n", data, len * 8);
+#endif
+    return data;
+  }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+#ifdef CONFIG_TRACE_MEM
+  log_write(" is out of bound.\n");
+#endif
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+#ifdef CONFIG_TRACE_MEM
+  log_write("Write Memory: *0x%x ", addr);
+#endif
+  if (likely(in_pmem(addr))) { 
+#ifdef CONFIG_TRACE_MEM
+  log_write("<= [ 0x%08lx ], length = %d\n", data, len * 8);
+#endif
+    pmem_write(addr, len, data); 
+    return;
+  }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+#ifdef CONFIG_TRACE_MEM
+  log_write(" is out of bound.\n");
+#endif
   out_of_bound(addr);
 }
